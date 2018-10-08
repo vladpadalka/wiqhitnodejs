b98f8aa8820532fb30a684f29d67024f15f33181
vladislav_padalka_hys <vladislav.padalka@hys-enterprise.com>
2018-10-08 14:52:12 +0300

    tests

--- config/config.js
+++ config/config.js
@@ -12,8 +12,11 @@ CONFIG.db_name      = process.env.DB_NAME       || 'name';
 CONFIG.db_user      = process.env.DB_USER       || 'root';
 CONFIG.db_password  = process.env.DB_PASSWORD   || '';
 
-CONFIG.jwt_encryption  = process.env.JWT_ENCRYPTION || 'jwt_please_change';
+CONFIG.jwt_encryption  = process.env.JWT_ENCRYPTION || 'please_change';
 CONFIG.jwt_expiration  = process.env.JWT_EXPIRATION || '10000';
-CONFIG.salt = process.env.SALT || 'SyUgag3GeHdQO4kOBMoB';
-    
+CONFIG.salt = process.env.SALT || 'please_change';
+
+CONFIG.test_user_email = process.env.TEST_USER_EMAIL || 'please_change';
+CONFIG.test_user_pw = process.env.TEST_USER_PW || 'please_change';
+
 module.exports = CONFIG;
--- test/user.js
+++ test/user.js
@@ -1,18 +1,21 @@
 //process.env.NODE_ENV = 'test';
 
-const app = require('../app');
-const { User } 	    = require('../models');
-
 //Require the dev-dependencies
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const should = chai.should();
-const apiVersionNum = '/v1';
-
 chai.use(chaiHttp);
+
+const app = require('../app');
+const { User } 	    = require('../models');
+const CONFIG            = require('../config/config');
+
+const apiVersionNum = '/v1';
 
 //Our parent block
 describe('Users', () => {
+
+	let userJwtToken;
 
 	beforeEach((done) => {
 		done();
@@ -20,19 +23,19 @@ describe('Users', () => {
 
 	describe('/POST users', () => {
 
-		it('it should create new user', (done) => {
+		it('create new user', (done) => {
 
 			User.destroy({
 				where: {
-					email : "vladislav.padalka+wiqhit3@hys-enterprise.com"
+					email : CONFIG.test_user_email
 				}
 			}).then(() => {
 
 				let user = {
 					first: "Vlad",
 					last: "Web",
-					email: "vladislav.padalka+wiqhit3@hys-enterprise.com",
-					password: "kTNzHtas4ZBU"
+					email: CONFIG.test_user_email,
+					password: CONFIG.test_user_pw
 				}
 				chai.request(app)
 					.post(apiVersionNum + '/users')
@@ -48,40 +51,37 @@ describe('Users', () => {
 	});
 
 	describe('/POST users/login', () => {
-		it('it should login a user', (done) => {
+		it('login a user', (done) => {
 			let user = {
-				email: "vladislav.padalka+wiqhit3@hys-enterprise.com",
-				password: "kTNzHtas4ZBU"
+				email: CONFIG.test_user_email,
+				password: CONFIG.test_user_pw
 			}
 			chai.request(app)
 				.post(apiVersionNum + '/users/login')
 				.send(user)
 				.end((err, res) => {
-					console.log("+++++++++1", res.body);
-					//res.should.have.status(201);
+					res.should.have.status(200);
+					userJwtToken = res.body.token;
 					done();
 				});
 		});
 	});
 
-	/*
 	describe('/GET users', () => {
-		it('it should get specified user', (done) => {
+		it('get specified user info', (done) => {
 			chai.request(app)
 		  .get(apiVersionNum + '/users')
-		  .set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxNzE0LCJpYXQiOjE1Mzg5ODI2MDksImV4cCI6MTUzODk5MjYwOX0.5yr0728xf5BTy1YxrA-RBFS1Oq4-jpVa5Y3-rvppcHE')
+		  .set('Authorization', userJwtToken)
 			.end((err, res) => {
 				res.should.have.status(200);
 				res.body.user.should.be.a('object');
 				res.body.success.should.be.eql(true);
-				res.body.user.have.property('email');
-				res.body.user.email.should.be.eql('vladislav.padalka+wiqhit3@hys-enterprise.com');
+				res.body.user.should.have.property('email');
+				res.body.user.email.should.be.eql(CONFIG.test_user_email);
 				done();
 			});
 		});
 	});
-	*/
-
 
 });
   
